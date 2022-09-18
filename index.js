e6a96a4558159841c211e433ea6bbb17c62ef439
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-09-19 04:54:04 +0600

    basic setup

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-import express from "express"
-const app = express()
+const express = require("express");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
+
+app.get('/', (req, res) => {
+    res.send("Booking App Connected")
+});
+app.listen(port, () => {
     console.log('connected')
 })
